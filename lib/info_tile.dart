df3ee24a83d39759b1b3b287842da74c66fa53ac
sg23x <soumyagupta2307@gmail.com>
2021-07-06 01:00:12 +0530

    modified: info tile

--- lib/info_tile.dart
+++ lib/info_tile.dart
@@ -3,10 +3,20 @@ import 'package:flutter/material.dart';
 class InfoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(20),
-      height: MediaQuery.of(context).size.height * 0.1,
-      color: Colors.pink,
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minWidth: MediaQuery.of(context).size.width * 1,
+        maxWidth: MediaQuery.of(context).size.width * 1,
+        minHeight: MediaQuery.of(context).size.height * 0.15,
+      ),
+      child: Container(
+        margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(width: 5, color: Colors.grey),
+        ),
+        child: Text(''),
+      ),
     );
   }
 }
